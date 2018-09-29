8bfab04d6deaac6ee17b348374ad94d12beb193b
Cassiano L. Trezzi <pcdotrezzi@outlook.com>
2018-09-29 09:18:49 -0300

    add10
    Estilizando paginas

--- equipe09/controle_doenca.php
+++ equipe09/controle_doenca.php
@@ -10,8 +10,8 @@
 	 exit();
   }
   
-  $btnExcluir = "http://inf.fw.iffarroupilha.edu.br/~bruno/disciplinas/programacao_web1/excluir.png";
-  $btnAlterar = //"http://inf.fw.iffarroupilha.edu.br/~bruno/disciplinas/programacao_web1/alterar.png";
+  $btnExcluir = "fotos/editar.jpg";
+  $btnAlterar = "fotos/excluir.jpg";
 
 
   $mensagem = "";
@@ -138,8 +138,8 @@
 		
 		$tabela = "<table border='4'>";
 		
-		$tabela = $tabela."<tr><th>Número da doença</th><th>Data da doença</th>
-		             <th>Alterar</th>Medicamento Usado<th>Excluir</th></tr>";
+		$tabela = $tabela."<tr><th>Número da doença</th><th>Data da doença</th><th>Medicamento Usado</th>
+		             <th>Alterar</th><th>Excluir</th></tr>";
 		 
 		while ( $dados = mysqli_fetch_assoc($lista) ) {
 		   
@@ -176,32 +176,43 @@
 <head>
 	<title>Cadastro das doenças </title>
 	<meta charset="utf-8" />
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </head>
 
 <body>
 	<table>
-	<h2>Página para cadastro das Doenças </h2>
+	<th>
+		<div class="alert alert-primary" role="alert">Controle de Doenças</div>
+	</th>
 	
 	<?php echo $mensagem; ?>
-	
-	
-		<legend>Dados das doenças:</legend>
-		
-		<form action="controle_doenca.php" method="post">
-		
-		Código da doença :  <input type="text" name="iddoenca" value="<?php echo $iddoenca; ?>"> <br><br>
-		Data da doença:     <input type="text" name="ocorrenciadoenca" value="<?php echo $ocorrenciadoenca; ?>" size="40"> <br>
-		Medicamento Usado:     <input type="text" name="medicamento" value="<?php echo $medicamento; ?>" size="40"> <br>
-		
-		<br><br>
-		
-		<input type="submit" value="Novo">
-		<input type="submit" name="acao" value="<?php echo $descr_acao; ?>">      
-		      
-		</form>
-	
-	
+	<tr>
+		<td>
+<div class="container-fluid" height="50px">
 
+		<form action="controle_doenca.php" method="post">
+  <div class="form-group">
+    <label for="formGroupExampleInput">Código da doença:</label>
+    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="" name="iddoenca" value="<?php echo $iddoenca; ?>" size="50%">
+  </div>
+  <div class="form-group">
+    <label for="formGroupExampleInput2">Data da Ocorrencia</label>
+    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="" name="ocorrenciadoenca" value="<?php echo $ocorrenciadoenca; ?>" size="50%">
+  </div>
+  <div class="form-group">
+    <label for="formGroupExampleInput2">Medicamento</label>
+    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="" name="medicamento" value="<?php echo $medicamento; ?>" size="50%">
+  </div>
+  <div>
+  	<button type="submit" class="btn btn-primary" value="Novo">Novo</button>
+  	<button type="submit" class="btn btn-primary" name="acao" value="<?php echo $descr_acao; ?>"><?php echo "$descr_acao"; ?></button>
+  </div>
+</div>
+</form>
+	
 	
 	<legend>Cadastrados</legend>
 	
@@ -210,7 +221,8 @@
 	
 
 	<br><br>
-	<hr>
+	</td>
+</tr>
 	</table>
 
 </body>
